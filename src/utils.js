ef4b005e02b666f419aa2388713d2b7797720e55
kijima <norimaking777@gmail.com>
2021-04-04 22:42:48 +0900

    Divide code

---
+++ src/utils.js
@@ -0,0 +1,31 @@
+function getStartTs(val) {
+  var start_date = new Date(val);
+  start_date.setHours(0);
+  start_date.setMinutes(0);
+  start_date.setSeconds(0);
+  start_date.setMilliseconds(0);
+  var start_ts = start_date.getTime() / 1000;
+  return start_ts;
+}
+
+function getEndTs(val) {
+  var end_date = new Date(val);
+  end_date.setHours(23);
+  end_date.setMinutes(59);
+  end_date.setSeconds(59);
+  end_date.setMilliseconds(0);
+  var end_ts = end_date.getTime() / 1000;
+  return end_ts;
+}
+
+function unixTime2ymd(intTime) {
+  var d = new Date(intTime * 1000);
+  var year = d.getFullYear();
+  var month = d.getMonth() + 1;
+  var day = d.getDate();
+  var hour = ('0' + d.getHours()).slice(-2);
+  var min = ('0' + d.getMinutes()).slice(-2);
+  var sec = ('0' + d.getSeconds()).slice(-2);
+
+  return (year + '/' + month + '/' + day + ' ' + hour + ':' + min + ':' + sec);
+}
