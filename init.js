a6573b47e69c2e259114a2f75827c310eb316341
kijima <norimaking777@gmail.com>
2021-04-04 16:46:34 +0900

    Delete init

--- init.js
+++
@@ -1,4 +0,0 @@
-function setProperty() {
-  PropertiesService.getScriptProperties().setProperty("SLACK_TOKEN","xxxx-0000-...");
-  PropertiesService.getScriptProperties().setProperty("CHANNEL_ID","ABC...");
-}
