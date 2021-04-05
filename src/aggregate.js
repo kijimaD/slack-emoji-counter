2e74c5b11e38a8bb3c6f0a96d109e62d61774d5d
kijima <norimaking777@gmail.com>
2021-04-05 23:51:53 +0900

    Add 3-dimensional array

--- src/aggregate.js
+++ src/aggregate.js
@@ -1,37 +1,42 @@
 function load() {
-  var data = [];
-
   var rows = SHEET.getDataRange();
   var numRows = rows.getNumRows();
   var values = rows.getValues();
+
+  var data = [];
+  // exclude a header line
   for (var i = 1; i <= numRows - 1; i++) {
     var row = values[i];
     data.push(row);
   }
 
-  Logger.log("今月のランキング発表のときがやってきました。")
-
+  // initialize 3-dimensional array
   var rankings = new Array(REACTIONS.length)
   for(var y = 0; y < 4; y++) {
     rankings[y] = [,,]
   }
+  // [[['10pt', 'msg'], ['5pt', 'msg'], ['1pt', 'msg']],
+  //  [['10pt', 'msg'], ['5pt', 'msg'], ['1pt', 'msg']],
+  //  [['10pt', 'msg'], ['5pt', 'msg'], ['1pt', 'msg']],
+  //  [['10pt', 'msg'], ['5pt', 'msg'], ['1pt', 'msg']]]
 
   for (var i = 0; i <= REACTIONS.length - 1; i++) {
-    Logger.log(REACTIONS[i] + "部門-+-+-+-+-+-+")
     data.sort(function(a, b){return(b[COLUMNS.length + i - 1] - a[COLUMNS.length + i - 1])})
     for (var j = 0; j <= 2; j++) {
-      Logger.log((j + 1) + "位:" + data[j][2] + " " + data[j][COLUMNS.length + i - 1] + "pts.");
-      rankings[i][j] = data[j][2]
+      rows = []
+      rows.push(String(j + 1))
+      rows.push(data[j][2])
+      rows.push(data[j][COLUMNS.length + i - 1])
+      rankings[i][j] = rows
     }
   }
   Logger.log(rankings)
+  return ranking
 }
 
 function ranking() {
-  [[['2pt', 'msg'], ['1pt', 'msg'], ['0pt', 'msg']],
-   [['2pt', 'msg'], ['1pt', 'msg'], ['0pt', 'msg']],
-   [['2pt', 'msg'], ['1pt', 'msg'], ['0pt', 'msg']],
-   [['2pt', 'msg'], ['1pt', 'msg'], ['0pt', 'msg']]]
+  Logger.log("今月のランキング発表のときがやってきました。")
+  Logger.log(REACTIONS[0] + "部門-+-+-+-+-+-+")
 }
 
 function post() {
