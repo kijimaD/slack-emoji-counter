e2064f05a91464717b7316d80ddd55907d640e8b
kijima <norimaking777@gmail.com>
2021-04-05 23:39:14 +0900

    Test

--- src/aggregate.js
+++ src/aggregate.js
@@ -4,24 +4,34 @@ function load() {
   var rows = SHEET.getDataRange();
   var numRows = rows.getNumRows();
   var values = rows.getValues();
-  for (var i = 0; i <= numRows - 1; i++) {
+  for (var i = 1; i <= numRows - 1; i++) {
     var row = values[i];
     data.push(row);
   }
 
   Logger.log("今月のランキング発表のときがやってきました。")
 
+  var rankings = new Array(REACTIONS.length)
+  for(var y = 0; y < 4; y++) {
+    rankings[y] = [,,]
+  }
+
   for (var i = 0; i <= REACTIONS.length - 1; i++) {
     Logger.log(REACTIONS[i] + "部門-+-+-+-+-+-+")
     data.sort(function(a, b){return(b[COLUMNS.length + i - 1] - a[COLUMNS.length + i - 1])})
-    for (var j = 1; j <= 3; j++) {
-      Logger.log(j + "位:" + data[j][2] + " " + data[j][COLUMNS.length + i - 1] + "pts.");
+    for (var j = 0; j <= 2; j++) {
+      Logger.log((j + 1) + "位:" + data[j][2] + " " + data[j][COLUMNS.length + i - 1] + "pts.");
+      rankings[i][j] = data[j][2]
     }
   }
+  Logger.log(rankings)
 }
 
 function ranking() {
-
+  [[['2pt', 'msg'], ['1pt', 'msg'], ['0pt', 'msg']],
+   [['2pt', 'msg'], ['1pt', 'msg'], ['0pt', 'msg']],
+   [['2pt', 'msg'], ['1pt', 'msg'], ['0pt', 'msg']],
+   [['2pt', 'msg'], ['1pt', 'msg'], ['0pt', 'msg']]]
 }
 
 function post() {
