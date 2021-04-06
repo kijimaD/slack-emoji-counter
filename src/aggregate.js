659f3dba01d7f66dc9420ee23f6fe32a66253a3a
kijima <norimaking777@gmail.com>
2021-04-06 22:03:43 +0900

    Add sending message

--- src/aggregate.js
+++ src/aggregate.js
@@ -13,32 +13,20 @@ function loadSheet() {
 }
 
 function rankingArray(data) {
-  // [[['10pt', 'msg'], ['5pt', 'msg'], ['1pt', 'msg']],
-  //  [['10pt', 'msg'], ['5pt', 'msg'], ['1pt', 'msg']],
-  //  [['10pt', 'msg'], ['5pt', 'msg'], ['1pt', 'msg']],
-  //  [['10pt', 'msg'], ['5pt', 'msg'], ['1pt', 'msg']]]
-
-  // initialize 3-dimensional array
-  var rankings = new Array(REACTIONS.length)
-  for(var y = 0; y < 4; y++) {
-    rankings[y] = [,,]
-  }
-
+  var msg = "";
   for (var i = 0; i <= REACTIONS.length - 1; i++) {
+    msg += (":" + REACTIONS[i] + ":" + REACTIONS_ALIAS[i] + "━━━━━━" + "\n")
     data.sort(function(a, b){return(b[COLUMNS.length + i - 1] - a[COLUMNS.length + i - 1])})
     for (var j = 0; j <= 2; j++) {
-      rows = []
-      rows.push(String(j + 1))
-      rows.push(data[j][2])
-      rows.push(data[j][COLUMNS.length + i - 1])
-      rankings[i][j] = rows
+      msg += (String(j + 1) + "位: " + data[j][COLUMNS.length + i - 1] + "pts " + data[j][2]+ "\n")
     }
+    msg += "\n"
   }
-  Logger.log(rankings);
-  return rankings;
+  return msg;
 }
 
 function notify() {
-  Logger.log("今月のランキング発表のときがやってきました。")
-  Logger.log(REACTIONS[0] + "部門-+-+-+-+-+-+")
+  data = loadSheet();
+  rankings = rankingArray(data);
+  Logger.log(msg);
 }
